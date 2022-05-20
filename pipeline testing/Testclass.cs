bba9ac13918acaa1b9e02049248002b06808d598
Gordon <tangy3933@gmail.com>
2022-05-20 15:30:48 +0930

    add more tests cases

--- pipeline testing/Testclass.cs
+++ pipeline testing/Testclass.cs
@@ -2,6 +2,14 @@ using Xunit;
 
 public class Testclass
 {
+    [Theory]
+    [InlineData(3)]
+    [InlineData(5)]
+    public void passingIsOdd(int value)
+    {
+        Assert.True(Program.IsOdd(value));
+    }
+
     [Fact]
     public void PassingAddTest()
     {
